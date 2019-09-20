dd695e8db9f6abfbcd5f802c312baed944c90989
chasegawa <chasegawa@unicon.net>
2019-09-20 13:42:25 -0700

    NOJIRA
    Adjustments to deps and code cleanup

--- src/main/java/com/instructure/services/ApplicationBootstrap.java
+++ src/main/java/com/instructure/services/ApplicationBootstrap.java
@@ -1,14 +1,10 @@
 package com.instructure.services;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpClientErrorException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @ComponentScan(basePackages = { "org.instructure" })
 // Remove the following annotation if you want to have Spring Boot autoconfigure a datasource for you
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@Slf4j // We use LOMBOK to generate a "log" object
+//@Slf4j // We use LOMBOK to generate a "log" object
 public class ApplicationBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationBootstrap.class, args);
